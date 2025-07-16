8056ae988c4c0c1f4ba54e4765ae2289b849d13a
lschloegel <lars.schloegel@gmail.com>
2025-07-16 20:54:02 +0200

    ex1

---
+++ 01-learn-python-the-hard-way/ex1.py
@@ -0,0 +1,12 @@
+# print("Hello World!")
+# print("Hello Again")
+# print("I like typing this.")
+# print("This is fun.")
+# print('Yay! Printing.')
+# print("I'd much rather you 'not'.")
+# print('I "said" do not touch this.')
+
+#study drills
+print("The hard way is the best way!")#
+
+# a octothorpe, pound, hash, mesh is an identifier for a comment
