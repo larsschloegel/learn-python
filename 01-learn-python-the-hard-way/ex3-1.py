ff0f0e40af8d045f103903741d02b3e8f509a2aa
lschloegel <lars.schloegel@gmail.com>
2025-07-20 13:20:13 +0200

    ex3

---
+++ 01-learn-python-the-hard-way/ex3-1.py
@@ -0,0 +1,6 @@
+# study drills
+x = input("Wie viele Programmiersprachen hast du bisher genutzt?")
+print(x)
+
+#floating numbers
+print("Hens", 25.0 + 30.0 / 6.0)
---
+++ 01-learn-python-the-hard-way/ex3.py
@@ -0,0 +1,36 @@
+# numbers and math
+
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+
+
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6) # Es wird 30 durch 6 geteillt und dann 25 addiert
+print("Roosters", 100 - 25 * 3 % 4) # 25 mal 3 ist 75, 75 modulo 4 ist 3, 100 - 3 ist 97
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
