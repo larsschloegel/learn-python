6480fa88b19de81f9c7dbebea2cdefcf3cdf6fc3
lschloegel <lars.schloegel@gmail.com>
2025-07-24 19:12:36 +0200

    ex11

---
+++ 01-learn-python-the-hard-way/ex11-user-input.py
@@ -0,0 +1,14 @@
+# print("How old are you?", end=' ')
+# age = input()
+# print("How tall are you?", end='')
+# height = input()
+# print("How much do you weight?", end=' ')
+# weight = input()
+
+# other way
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("So much do you weight?")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
