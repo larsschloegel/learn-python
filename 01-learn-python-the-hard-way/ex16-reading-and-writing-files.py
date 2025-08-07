82041aae04e85e5918ff9bac1c54b6e7a9c9eaf2
lschloegel <lars.schloegel@gmail.com>
2025-08-07 08:23:41 +0200

    ex16

--- 01-learn-python-the-hard-way/ex16-reading-and-writing-files.py
+++ 01-learn-python-the-hard-way/ex16-reading-and-writing-files.py
@@ -34,12 +34,14 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+content = f"{line1}\n{line2}\n{line3}\n"
+target.write(content)
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
 
 print("And finally, we close it.")
 target.close()
